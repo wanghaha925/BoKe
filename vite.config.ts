7ef4e458a5540f990cb2760dc3307ed446479ff6
wanghaha925 <150261677+wanghaha925@users.noreply.github.com>
2025-06-28 16:20:26 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  // GitHub Pages 部署配置
  // GitHub Pages 部署配置
 base: process.env.NODE_ENV === 'production' ? '/BoKe/' : '/',
   build: {
